32cf56fd31fcbc1345d4f34592932b0dc248455e
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-04-16 21:07:10 +0600

    no changes just problem with eslint

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import { useSelector } from 'react-redux'
 import './App.css'
+
 import { Header } from './containers/Header'
 import { Quiz } from './containers/Quiz'
 
 function App() {
-	const state = useSelector(state => state)
-	console.log(state);
-	return (
-		<div className='App'>
-			<Header />
-			<Quiz />
-		</div>
-	)
+  const a = ''
+
+  const state = useSelector((state) => {
+    return state
+  })
+
+  console.log(state)
+  return (
+    <div className='App'>
+      <Header />
+      <Quiz />
+    </div>
+  )
 }
 
 export default App
--- src/components/Quiz/QuizMakerForm.jsx
+++ src/components/Quiz/QuizMakerForm.jsx
@@ -1,140 +1,143 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import { Input } from '../UI/Input'
 import { BiMove } from 'react-icons/bi'
 import { MdOutlineImage } from 'react-icons/md'
+import { useDispatch, useSelector } from 'react-redux'
+import { Input } from '../UI/Input'
 import { Select } from '../UI/Select'
-import { useState } from 'react'
+
 import { Modal } from '../UI/Modal'
 import { QustionTypes } from '../../data/QuestionTypes'
 import { TypeItem } from '../UI/TypeItem'
 import { QuizAnswerCreater } from './QuizAnswerCreater'
 import { AnswerInput } from '../UI/AnswerInput'
 import { FooterQuizCreaterForm } from './FooterQuizCreaterForm'
-import { useDispatch, useSelector } from 'react-redux'
 import { testCreatorActions } from '../../store/testCreatorSlice'
 import { useInput } from '../../hooks/useInput'
 
 const StyledQuizMakerForm = styled.form`
-	.header {
-		width: 760px;
-		display: flex;
-		justify-content: center;
-		margin-bottom: 30px;
-	}
-	.MoveIcon {
-		margin-top: 10px;
-		cursor: grab;
-	}
+  .header {
+    width: 760px;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 30px;
+  }
+  .MoveIcon {
+    margin-top: 10px;
+    cursor: grab;
+  }
 
-	.makers_firs_part {
-		width: 100%;
-		display: flex;
-		justify-content: space-around;
-		align-items: flex-end;
-		margin-bottom: 10px;
-	}
-	.inputWrapper {
-		width: 40%;
-	}
-	.answerList {
-		width: 100%;
-	}
+  .makers_firs_part {
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: flex-end;
+    margin-bottom: 10px;
+  }
+  .inputWrapper {
+    width: 40%;
+  }
+  .answerList {
+    width: 100%;
+  }
 `
 
 export const QuizMakerForm = ({ information }) => {
-	const dispatch = useDispatch()
-	const [modal, ToggleModal] = useState(false)
-	const [type, setType] = useState(QustionTypes[0])
-	const questionValue = useInput('')
-	const toggleModal = (e) => {
-		e.preventDefault()
-		ToggleModal((prev) => !prev)
-		console.log(modal)
-	}
-	const addType = (obj) => {
-		setType(obj)
-		ToggleModal((prev) => !prev)
-	}
-	const createAnswer = () => {
-		dispatch(
-			testCreatorActions.createAnswer({
-				id: information.id,
-			}),
-		)
-	}
+  const dispatch = useDispatch()
+  const [modal, ToggleModal] = useState(false)
+  const [type, setType] = useState(QustionTypes[0])
+  const questionValue = useInput('')
+  const toggleModal = (e) => {
+    e.preventDefault()
+    ToggleModal((prev) => {
+      return !prev
+    })
+    console.log(modal)
+  }
+  const addType = (obj) => {
+    setType(obj)
+    ToggleModal((prev) => {
+      return !prev
+    })
+  }
+  const createAnswer = () => {
+    dispatch(
+      testCreatorActions.createAnswer({
+        id: information.id,
+      })
+    )
+  }
 
-	const ModalRendering = () => {
-		return (
-			<>
-				{modal && (
-					<Modal cancel={toggleModal}>
-						{QustionTypes.map((el) => {
-							return (
-								<TypeItem
-									onClick={() => addType(el)}
-									key={el.id}
-								>
-									<img width={17} src={el.icon} alt='' />
-									<p>{el.text}</p>
-								</TypeItem>
-							)
-						})}
-					</Modal>
-				)}
-			</>
-		)
-	}
+  const ModalRendering = () => {
+    return (
+      <>
+        {modal && (
+          <Modal cancel={toggleModal}>
+            {QustionTypes.map((el) => {
+              return (
+                <TypeItem
+                  onClick={() => {
+                    return addType(el)
+                  }}
+                  key={el.id}
+                >
+                  <img width={17} src={el.icon} alt='' />
+                  <p>{el.text}</p>
+                </TypeItem>
+              )
+            })}
+          </Modal>
+        )}
+      </>
+    )
+  }
 
-	const AnswerRendering = () => {
-		return (
-			<>
-				{information.answers.length > 0 &&
-					information.answers.map((el) => {
-						return (
-							<AnswerInput
-								type={type.type}
-								answerIcon={type.iconAnswer}
-								qwestionId={information.id}
-								answerId={el.id}
-								key={el.id}
-							/>
-						)
-					})}
-			</>
-		)
-	}
+  const AnswerRendering = () => {
+    return (
+      <>
+        {information.answers.length > 0
+          && information.answers.map((el) => {
+            return (
+              <AnswerInput
+                type={type.type}
+                answerIcon={type.iconAnswer}
+                qwestionId={information.id}
+                answerId={el.id}
+                key={el.id}
+              />
+            )
+          })}
+      </>
+    )
+  }
 
-	return (
-		<StyledQuizMakerForm>
-			<header className='header'>
-				<BiMove className='MoveIcon' fontSize={22} />
-			</header>
-			<section className='makers_firs_part'>
-				<div className='inputWrapper'>
-					<Input
-						value={questionValue.value}
-						onChange={(e) => questionValue.onChange(e)}
-						placeholder='question without title'
-						width={100}
-						fontsize={14}
-					/>
-				</div>
-				<MdOutlineImage fontSize={25} />
-				<Select
-					type={type}
-					onClick={toggleModal}
-					height={40}
-					width={40}
-				/>
+  return (
+    <StyledQuizMakerForm>
+      <header className='header'>
+        <BiMove className='MoveIcon' fontSize={22} />
+      </header>
+      <section className='makers_firs_part'>
+        <div className='inputWrapper'>
+          <Input
+            value={questionValue.value}
+            onChange={(e) => {
+              return questionValue.onChange(e)
+            }}
+            placeholder='question without title'
+            width={100}
+            fontsize={14}
+          />
+        </div>
+        <MdOutlineImage fontSize={25} />
+        <Select type={type} onClick={toggleModal} height={40} width={40} />
 
-				{ModalRendering()}
-			</section>
-			<div className='answerList'>
-				{AnswerRendering()}
-				<QuizAnswerCreater onClick={createAnswer} />
-			</div>
-			<FooterQuizCreaterForm qwestionId={information.id} />
-		</StyledQuizMakerForm>
-	)
+        {ModalRendering()}
+      </section>
+      <div className='answerList'>
+        {AnswerRendering()}
+        <QuizAnswerCreater onClick={createAnswer} />
+      </div>
+      <FooterQuizCreaterForm qwestionId={information.id} />
+    </StyledQuizMakerForm>
+  )
 }
