746132856b8a915f5946703f665a56e1df217b1f
jetigen2003 <abdillaevjetigen127@gmail.com>
2022-04-19 18:39:46 +0600

    merge quiz feature and development

--- src/components/Quiz/AnswerInput.jsx
+++ src/components/Quiz/AnswerInput.jsx
@@ -1,4 +1,4 @@
-import React, { useTransition, useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { GrFormClose } from 'react-icons/gr'
 import { useDispatch } from 'react-redux'
